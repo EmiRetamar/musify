c0d34ced19bd7bd51deae4fe0821e16a8d94568c
Emi Retamar <emiliano_retamar@hotmail.com.ar>
2019-08-22 18:47:35 -0300

    Cambio la url base de la api por la de musify-api que esta en Heroku

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,5 +1,5 @@
 export var GLOBAL = {
-    url: 'https://musify-application.herokuapp.com/api/',
+    url: 'https://musify-app-api.herokuapp.com/api/',
     localhost: 'http://localhost:3977/api/',
     ip: '127.0.0.1'
 }
